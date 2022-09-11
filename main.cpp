ea242bcada84815960acffe36421aacb540d1a69
AntoDono <anthony20151128@gmail.com>
2022-09-11 10:27:11 -0400

    minimum

--- main.cpp
+++ main.cpp
@@ -16,27 +16,26 @@ bool debug = true;
 
 int main(){
 
-    cout << "Dynamic Solver - Youwei Zhen" << endl;
-    cout << "This program can calculate the total number of ways you can make up any amount of money." << endl;
+    cout << "Minimum Coins - Youwei Zhen" << endl;
+    cout << "This program can calculate the minimum amount of coins to make up a given amount of money." << endl;
     cout << "Please enter any amount $: ";
     cin >> input;
 
     float normalized = input * 100.0; // makes xx.xx become xxxx, where the last two x represents the cents
     target = (int) normalized;
 
-    dp[0] = 1;
+    dp[0] = 0; // there are 0 ways to make $0.00 using any of the coins
 
     for (int amt = 1; amt <= target; amt++){
+        dp[amt] = numeric_limits<long long>::max(); // sets dp[amt] to max value
         for (int cvalue : coins){ // current coin we are using to add to exisiting amt
             if (amt - cvalue >= 0){
-                if (debug) cout << dp[amt - cvalue] << " ways to make $ (cents)" << amt - cvalue << endl;
-                if (debug) cout << dp[amt] << " ways to make $ (cents)" << amt << endl;
-                dp[amt] += dp[amt-cvalue];
+                if (debug) cout << "$(cents)" << amt - cvalue << ": " << dp[amt-cvalue] << endl;
+                dp[amt] = min(dp[amt-cvalue] + 1, dp[amt]);
             }
         }
-        if (debug) cout << "$ (cents)" << amt << ": " << dp[amt] << endl;
-        if (debug) cout << endl;
+        if (debug) cout << "$(cents)" << amt << ": " << dp[amt] << endl;
     }
 
-    cout << "Total ways to make up $" << input << ": " << dp[target] << endl;
+    cout << "Minimum amount of coins to make up $" << input << ": " << dp[target] << endl;
 }
