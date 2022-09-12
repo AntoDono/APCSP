8c7be43e5e964ca11635f85d6e4dbd3a1c48cda4
Anthony Zhen <anthony20151128@gmail.com>
2022-09-12 13:16:34 -0400

    d3

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+var simulations = []
+
 const coinFlip = ()=>{
     return Math.random() > 0.5
 }
 
 const simulate = ()=> {
-    for (let i = 0; i < 50; i ++){
-        console.log(coinFlip())
+    let total = 50, heads = 0, tails = 0
+    for (let i = 0; i < total; i ++){
+        if (coinFlip()) heads++
+        else tails++
     }
+    let percent = heads/total
+    simulations.push(percent)
+    console.log(simulations)
+    document.getElementById("result").innerHTML = `Simulated ${total} coin flips. Heads: ${percent}`
 }
+
+let graph = d3.select('graph')
+console.log(graph)
